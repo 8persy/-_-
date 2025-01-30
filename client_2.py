c84df3b3b020c4a1b336e8c0a73034daa00d366c
8percy8 <izergin.v05@mail.ru>
2025-01-30 13:42:38 +0300

    add except

--- client_2.py
+++ client_2.py
@@ -19,15 +19,18 @@ class Client:
                 print('error with send ', e)
 
     def receive(self):
-        while True:
-            data = self.sock.recv(1024)
-            message = pickle.loads(data)
+        try:
+            while True:
+                data = self.sock.recv(1024)
+                message = pickle.loads(data)
 
-            if message.get('stats', ''):
-                print("stats: ", message.get('stats'))
-                continue
+                if message.get('stats', ''):
+                    print("stats: ", message.get('stats'))
+                    continue
 
-            print(message.get('msg', ''))
+                print(message.get('msg', ''))
+        except Exception as e:
+            print('exception ', e)
 
     def connect(self):
         self.sock.connect(('127.0.0.1', 65432))
